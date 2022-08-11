df79a739b655766f1996cd3d28f1402daafe4ad7
evenm88 <evenkarthick@gmail.com>
2022-08-11 18:41:26 +0530

    Minor text change

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.send("Hi all");
+      res.send("Hi everyone !!!");
       req.statusCode=200;
       res.statusMessage = "Hi all";
     } catch (error) {
